a967cfe04bfc055f23cf909eb59ee17ffc876f10
alternativelowtier <marian06santana@gmail.com>
2026-05-04 14:19:46 -0400

    Create game.js

---
+++ assets/scripts/core/game.js
@@ -0,0 +1,16 @@
+class ParticleEffect { ... }
+function spawnHitSparks(...) { ... }
+function drawHollowPurple(...) { ... }
+const bfMinigame = { ... };
+class CombatProjectile { ... }
+const CharacterStatsBase = { ... };
+class Fighter { ... }
+const domainExpansionSystem = { ... };
+const hollowPurpleSystem = { ... };
+const domainClashSystem = { ... };
+function startHollowPurpleSequence(...) { ... }
+function triggerUltimate(...) { ... }
+function triggerDomainExpansion(...) { ... }
+function drawDomainBackground(...) { ... }
+// ... showCinematicMessage, drawCinematicIntros
+// ... gameLoop, triggerMatchEnd, rematch, startGameplayLoop, startGameEngine
