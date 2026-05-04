18fac7de587ebc8ed55b76aae6f5cb054e4cace4
alternativelowtier <marian06santana@gmail.com>
2026-05-04 14:22:19 -0400

    Create mechanics.js

---
+++ assets/scripts/systems/mechanics.js
@@ -0,0 +1,10 @@
+const bfMinigame = { ... };
+const domainExpansionSystem = { ... };
+const hollowPurpleSystem = { ... };
+const domainClashSystem = { ... };
+
+function startHollowPurpleSequence(player) { ... }
+function triggerUltimate(player) { ... }
+function triggerDomainExpansion(player) { ... }
+function drawDomainBackground(ctx, type, timer) { ... }
+function drawCinematicIntros(ctx, t) { ... }
